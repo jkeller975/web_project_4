db4f08f9cc4d20a6135083419eddfa7368738002
Josh Keller <JSKeller975@gmail.com>
2022-04-12 12:29:44 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Project 4: Around The U.S.
+
+## Project features
+
+- Semantic HTML5
+- Flexbot
+- Grid
+- Responsive design
+- Flat BEM
+- Custom Form
+- JavaScript
+
+### Overview
+
+- Figma
+- GitHub Page
+
+**Figma**
+
+- [Link to the project on Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+
+**GitHub Page**
+
+- View the project at the following link: [Link to the final webpage on GitHub](https://jkeller975.github.io/web_project_4/)
---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const modalWindow = document.querySelector(".popup");
+const editForm = document.querySelector(".popup__form");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = modalWindow.querySelector(".popup__close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const nameInputField = editForm.querySelector(".popup__input_type_name");
+const descriptionInputField = editForm.querySelector(
+  ".popup__input_type_description"
+);
+
+function removeModalVisibility() {
+  modalWindow.classList.remove("popup__opened");
+}
+
+function addModalVisibility() {
+  modalWindow.classList.add("popup__opened");
+  nameInputField.value = profileName.textContent;
+  descriptionInputField.value = profileDescription.textContent;
+}
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+  profileName.textContent = nameInputField.value;
+  profileDescription.textContent = descriptionInputField.value;
+  removeModalVisibility();
+}
+
+editForm.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", addModalVisibility);
+closeButton.addEventListener("click", removeModalVisibility);
