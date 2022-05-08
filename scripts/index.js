0e8fee9c9e6a087df91861af4074ea690480e95b
Josh Keller <JSKeller975@gmail.com>
2022-05-08 12:57:49 -0400

    Project 5 start

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
+const placesWrap = document.content.querySelector(".elements__list");
 const modalWindow = document.querySelector(".popup");
 const editForm = document.querySelector(".popup__form");
 const editButton = document.querySelector(".profile__edit-button");
@@ -29,3 +60,19 @@ function formSubmitHandler(e) {
 editForm.addEventListener("submit", formSubmitHandler);
 editButton.addEventListener("click", addModalVisibility);
 closeButton.addEventListener("click", removeModalVisibility);
+
+const getCardElement = (data) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector(".card__item").src = `url("${data.link}")`;
+  cardElement.querySelector(".card__text").textContent = data.name;
+  return cardElement;
+};
+
+const renderCard = (data, wrapper) => {
+  const newCard = getCardElement(data);
+  wrapper.prepend(newCard);
+};
+
+initialCards.forEach((data) => {
+  renderCard(data, placesWrap);
+});
